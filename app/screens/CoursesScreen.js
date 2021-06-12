8aa11f0fcca4f7cbd9a8139f3f80e412b7468b54
Michal Zaporski <focus16k@gmail.com>
2021-06-12 20:42:15 +0200

    navigation and categories

---
+++ app/screens/CoursesScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { URL } from "../restApiUrl.js";
+
+export default function CoursesScreen() {
+  return (
+    <View>
+      <Text>SIEMA</Text>
+    </View>
+  );
+}
