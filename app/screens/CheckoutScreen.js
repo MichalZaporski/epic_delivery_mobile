d248229d731d1c5793dcea247b90da278cc691c3
Michal Zaporski <focus16k@gmail.com>
2021-06-25 18:05:25 +0200

    post order

--- app/screens/CheckoutScreen.js
+++ app/screens/CheckoutScreen.js
@@ -10,59 +10,142 @@ import {
   TextInput,
   Image,
   Button,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
+import numberToCurrency from "../helpers/NumberToCurrency";
 import { URL } from "../restApiUrl.js";
 import { Formik } from "formik";
 import colors from "../styles/colors.js";
 
 export default function CheckoutScreen({ route, navigation }) {
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
   const params = route.params;
   const [items, setItems] = useState(params.cart);
+  const API_URL = URL;
+
+  const handleSubmit = (values) => {
+    setIsPending(true);
+
+    fetch(`${API_URL}/api/v1/orders`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        setIsPending(false);
+        if (!response.ok) throw new Error(response.status);
+        else console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      });
+  };
+
+  const displayCourses = (cart, courses) => {
+    const display_text = courses.map((course) => {
+      if (course.id in cart) {
+        return (
+          <View style={styles.singularCourse}>
+            <Text style={styles.courseText}>
+              {cart[course.id]} x: {course.name} -{" "}
+              {numberToCurrency(course.price * cart[course.id])}
+            </Text>
+          </View>
+        );
+      }
+    });
+    return display_text;
+  };
+
+  const countCummary = (cart, courses) => {
+    let sum = 0;
+    for (const course of courses) {
+      if (course.id in cart) {
+        sum += course.price * cart[course.id];
+      }
+    }
+    return sum;
+  };
 
   return (
-    <View style={{ flex: 1 }}>
-      <Text>{JSON.stringify(params.cart)}</Text>
+    <View style={styles.orderContainer}>
+      {displayCourses(params.cart, params.courses)}
+      <View style={styles.summaryView}>
+        <Text style={styles.summaryText}>
+          Summary: {numberToCurrency(countCummary(params.cart, params.courses))}
+        </Text>
+      </View>
+
+      <Text style={styles.textOne}>Fill in the details!</Text>
       <Formik
+        style={{ flex: 1 }}
         initialValues={{
-          city: "",
-          street: "",
-          street_number: "",
-          phone_number: "",
+          order: {
+            city: "",
+            street: "",
+            street_number: "",
+            phone_number: "",
+            note: "",
+            items: params.cart,
+          },
         }}
-        onSubmit={(values) => console.log(values)}>
+        onSubmit={(values) => handleSubmit(values)}>
         {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <View style={styles.orderContainer}>
+          <View style={styles.form}>
             <TextInput
               style={styles.orderInput}
-              onChangeText={handleChange("city")}
+              onChangeText={handleChange("order.city")}
               placeholder="City name"
-              value={values.city}
+              value={values.order.city}
             />
             <TextInput
               style={styles.orderInput}
-              onChangeText={handleChange("street")}
+              onChangeText={handleChange("order.street")}
               placeholder="Street name"
-              value={values.street}
+              value={values.order.street}
             />
             <TextInput
               style={styles.orderInput}
-              onChangeText={handleChange("street_number")}
+              onChangeText={handleChange("order.street_number")}
               placeholder="Street number"
-              value={values.street_number}
+              value={values.order.street_number}
             />
             <TextInput
               style={styles.orderInput}
-              onChangeText={handleChange("phone_number")}
+              onChangeText={handleChange("order.phone_number")}
               placeholder="Phone number"
-              value={values.phone_number}
+              value={values.order.phone_number}
+            />
+            <TextInput
+              style={styles.orderInput}
+              onChangeText={handleChange("order.note")}
+              placeholder="Note for your order"
+              value={values.order.note}
             />
 
-            <TouchableOpacity
-              style={styles.submit}
-              onPress={handleSubmit}
-              title="Order">
-              <Text style={styles.submitText}>Order</Text>
-            </TouchableOpacity>
+            {!isPending && (
+              <TouchableOpacity
+                style={styles.submit}
+                onPress={handleSubmit}
+                title="Order">
+                <Text style={styles.submitText}>Order</Text>
+              </TouchableOpacity>
+            )}
+
+            {isPending && (
+              <TouchableOpacity style={styles.submit}>
+                <Text style={styles.submitText}>Ordering...</Text>
+              </TouchableOpacity>
+            )}
+
+            {error && (
+              <Text style={styles.courseText}>
+                Something went wrong... try again later.
+              </Text>
+            )}
           </View>
         )}
       </Formik>
@@ -74,6 +157,48 @@ const styles = StyleSheet.create({
   orderContainer: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  singularCourse: {
+    width: "90%",
+    backgroundColor: colors.backgroundWhite,
+    paddingLeft: 30,
+    paddingVertical: 4,
+    marginTop: 15,
+    borderWidth: 2,
+    borderColor: colors.mainMaroon,
+    borderRadius: 4,
+  },
+  courseText: {
+    color: colors.fontGray,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  summaryView: {
+    width: "100%",
+    backgroundColor: colors.backgroundWhite,
+    paddingLeft: 30,
+    paddingVertical: 4,
+    marginTop: 15,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    borderColor: colors.mainMaroon,
+  },
+  summaryText: {
+    color: colors.fontGray,
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  textOne: {
+    marginTop: 15,
+    color: colors.fontGray,
+    fontSize: 22,
+    fontWeight: "600",
+  },
+  form: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
   },
   orderInput: {
     width: "80%",
@@ -81,6 +206,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginVertical: 10,
     padding: 5,
+    backgroundColor: colors.backgroundWhite,
   },
   submit: {
     width: "80%",
